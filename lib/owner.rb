f4ad1ff1832e96201a41fe6f8f62678d6fbed499
Amanda King <amapickell@gmail.com>
2019-07-16 20:13:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -17,33 +17,33 @@ class Owner
   
   def buy_cat (name)
     new_cat = Cat.new(name, self)
-    @cats << new_cat
+    self.cats << new_cat
   end
   
   def buy_dog (name)
     new_dog = Dog.new(name, self)
-    @dogs << new_dog
+    self.dogs << new_dog
   end
   
   def walk_dogs
-    @dogs.each {|dog| dog.mood = "happy"}
+    self.dogs.each {|dog| dog.mood = "happy"}
   end
   
   def feed_cats
-    @cats.each {|cat| cat.mood = "happy"}
+    self.cats.each {|cat| cat.mood = "happy"}
   end
   
   def sell_pets
-    @dogs.each do |dog| 
+    self.dogs.each do |dog| 
       dog.mood = "nervous" 
       dog.owner = nil
     end
-    @cats.each do |cat| 
+    self.cats.each do |cat| 
       cat.mood = "nervous" 
       cat.owner = nil
     end
-    @dogs.clear
-    @cats.clear
+    self.dogs.clear
+    self.cats.clear
   end
   
   def list_pets
